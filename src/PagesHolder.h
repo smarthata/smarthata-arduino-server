62df68d224069e4078234c92afba0b1e6bbfeef5
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2017-04-28 23:48:19 +0300

    Add ArduinoJson lib

--- src/PagesHolder.h
+++ src/PagesHolder.h
@@ -1,32 +1,57 @@
 #ifndef SMARTHATA_PAGESHOLDER_H
 #define SMARTHATA_PAGESHOLDER_H
 
+#include <ArduinoJson.h>
+#include <MemoryFree.h>
 #include "Pageable.h"
 
-FunctionPage msPage = FunctionPage("/ms", []() { return String(millis()); });
-FunctionPage mcsPage = FunctionPage("/mcs", []() { return String(micros()); });
+FunctionPage dateTime = FunctionPage("/date-time.json", []() {
+    StaticJsonBuffer<100> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    root["date"] = "20 Апреля";
+    unsigned long time = millis() / 1000;
+    root["time"] = String(time / 60) + ":" + String(time % 60);
+    root["temperature"] = 0;
+    root["memory"] = freeMemory();
+
+    String result;
+    root.printTo(result);
+    return result;
+});
+
+FunctionPage wateringAlarms = FunctionPage("/watering-alarms.json", []() {
+    StaticJsonBuffer<1000> jsonBuffer;
+    JsonArray &root = jsonBuffer.createArray();
+
+    for (int i = 0; i < 4; ++i) {
+        JsonObject &object = root.createNestedObject();
+        object["time"] = String(i * 4 + 10) + ":" + String(i * 10 + 20);
+        object["enabled"] = i % 3 != 0;
+    }
+
+    String result;
+    root.printTo(result);
+    return result;
+});
 
 Page page_app_js = {app_js_name, app_js, app_js_len - 1};
-Page page_date_time_json = {date_time_json_name, date_time_json, date_time_json_len - 1};
 Page page_favicon_ico = {favicon_ico_name, favicon_ico, favicon_ico_len - 1};
 Page page_index_html = {index_html_name, index_html, index_html_len - 1};
 Page page_not_found_html = {not_found_html_name, not_found_html, not_found_html_len - 1};
 Page page_style_css = {style_css_name, style_css, style_css_len - 1};
 Page page_tiles_html = {tiles_html_name, tiles_html, tiles_html_len - 1};
-Page page_watering_alarms_json = {watering_alarms_json_name, watering_alarms_json, watering_alarms_json_len - 1};
 Page page_watering_html = {watering_html_name, watering_html, watering_html_len - 1};
 
 Pageable *const pages[] = {
-        &msPage,
-        &mcsPage,
+        &dateTime,
+        &wateringAlarms,
         &page_app_js,
-        &page_date_time_json,
         &page_favicon_ico,
         &page_index_html,
         &page_not_found_html,
         &page_style_css,
         &page_tiles_html,
-        &page_watering_alarms_json,
         &page_watering_html,
 };
 
